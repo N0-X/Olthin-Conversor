be5f0b5859fca9817447e3aa16b8e7f81cbb4ee7
IVB-N0x <129079897+N0-X@users.noreply.github.com>
2025-04-24 00:27:26 -0300

    Add files via upload

---
+++ CONVERSOROLTHIN/js/conversor.js
@@ -0,0 +1,68 @@
+let origem = null;
+let destino = null;
+
+const taxas = {
+    trocado: {
+        trocado: 1,
+        prata: 0.02,
+        dobrão: 0.0004,
+        platina: 0.000004
+    },
+    prata: {
+        trocado: 50,
+        prata: 1,
+        dobrão: 0.02,
+        platina: 0.0002
+    },
+    dobrão: {
+        trocado: 2500,
+        prata: 50,
+        dobrão: 1,
+        platina: 0.01
+    },
+    platina: {
+        trocado: 250000,
+        prata: 5000,
+        dobrão: 100,
+        platina: 1
+    }
+};
+
+function selecionarOrigem(moeda, botao) {
+    origem = moeda;
+    document.querySelectorAll(".botao-moeda").forEach(btn => {
+    if (btn.onclick.toString().includes("selecionarOrigem")) {
+        btn.classList.remove("botao-selecionado");
+    }
+    });
+    botao.classList.add("botao-selecionado");
+}
+
+function selecionarDestino(moeda, botao) {
+    destino = moeda;
+    document.querySelectorAll(".botao-moeda").forEach(btn => {
+    if (btn.onclick.toString().includes("selecionarDestino")) {
+        btn.classList.remove("botao-selecionado");
+    }
+    });
+    botao.classList.add("botao-selecionado");
+}
+
+function converter() {
+    const quantidade = parseFloat(document.getElementById("quantidade").value);
+    if (isNaN(quantidade)) {
+    document.getElementById("resultado").innerText = "Por favor, insira um valor válido.";
+    return;
+}
+
+if (!origem || !destino) {
+    document.getElementById("resultado").innerText = "Selecione a moeda de origem e destino.";
+    return;
+}
+
+const taxa = taxas[origem][destino];
+const resultado = quantidade * taxa;
+
+document.getElementById("resultado").innerText =
+    `${quantidade} ${origem.toUpperCase()} = ${resultado.toFixed(2)} ${destino.toUpperCase()}`;
+}
